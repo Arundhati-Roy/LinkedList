a67e5f8d4fab11708dd2449d6b08fd525b51e44f
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-21 12:02:13 +0530

    UC1 and UC2 Added

---
+++ LinkedList/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    class LinkedList
+    {
+        internal Node top;
+        public LinkedList()
+        { this.top = null; }
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+                this.top = node;
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is added into Linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            Console.WriteLine("Elements inside Linked List");
+            while (temp != null)
+            {
+                Console.Write("->"+temp.data );
+                temp = temp.next;
+            }
+        }
+    }
+}
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,6 +7,16 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Demo!");
+            LinkedList list = new LinkedList();
+
+            //UC1
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Display();
+            //Console.WriteLine("After insertion " + list.InsertAtGivenPos(2, 30));
+            //list.Display();
+
         }
     }
 }
